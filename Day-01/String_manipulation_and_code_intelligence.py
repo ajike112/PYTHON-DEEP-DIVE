217b52580231dacb0328b151bb688c6c0630606d
yourUsername <yourEmail@example.com>
2026-05-18 21:06:38 -0500

    Push everything to commit about python

---
+++ Day-01/String_manipulation_and_code_intelligence.py
@@ -0,0 +1,2 @@
+
+print("Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\nKnead the dough for 10 minutes.\nAdd 3g of Salt.\nLeave to rise for 2 hours.\nBake at 200 degrees C for 30 minutes.")
