59d31bb1db7add05eeae6136c13dfaf566835ffb
yourUsername <yourEmail@example.com>
2026-05-19 01:01:24 -0500

    Push everything to commit about python

---
+++ Day-01/Day_01_project.py
@@ -0,0 +1,9 @@
+
+print("Welcome to the Band Name Generator.")
+print(input("What's the name of the city you grew up in?" + " " + "What's your pet's name?"))
+
+# Another way to write this (Best way, actually)
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?")
+pet = input("What is the name of a pet?")
+print("Your band namne could be: " + city + " " + pet)
---
+++ Day-01/Variable_naming.py
@@ -0,0 +1,5 @@
+
+# Best practice to name as Python is written in plain English to understand
+name = "Adekunle"
+length = len(name)
+print(length)
